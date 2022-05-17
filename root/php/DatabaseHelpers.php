0d396cc4d8f7d35cb4707d61aee5ff3273e247bc
bilal asghar <asgharbilal8@gmail.com>
2022-05-17 16:47:43 +0100

    created function to get data from database

--- root/php/DatabaseHelpers.php
+++ root/php/DatabaseHelpers.php
@@ -3,7 +3,7 @@ function OpenConnection()
 {
     $server = "localhost";
     $username = "root";
-    $password = "";
+    $password = "Hoisaejfr^&o2";
     $database = "Vaccinations";
 
     $conn = new mysqli($server, $username, $password, $database);
@@ -13,5 +13,31 @@ function OpenConnection()
     }
 
     return $conn;
+}
+
+function CallDatabase($sql, $isDataReturned)
+{
+    try {
+        $conn = OpenConnection();
+
+        if ($isDataReturned) {
+            $result = mysqli_query($conn, $sql);
+
+            // we have data so store in variable then return
+            while ($row = mysqli_fetch_array($result)) {
+                $rows[] = $row;
+            }
+
+            mysqli_free_result($result); // free memory associated with result
+            $conn->close();
+            return $rows;
+        } else {
+            $result = mysqli_query($conn, $sql);
+            $conn->close();
+        }
+    } catch (Exception $e) {
+        $conn->close();
+        echo "Error!" . $e->getMessage();
+    }
 }
 ?>
